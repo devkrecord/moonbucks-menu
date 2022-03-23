fc53014bb2fa203b5dcbcdbc6010fb16b6af6294
dev.k <dev.krecord@gmail.com>
2022-03-23 19:35:51 +0900

    feat: 요구사항 분석 및 문벅스 카페 에스프레소 메뉴판 만들기 - 메뉴 수정

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,31 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  // TODO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+  // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+
+  // 이벤트 위임(Event Delegation)이란 동적으로 노드를 생성하고 삭제할 때 각 노드에 대해 이벤트를 추가 하지 않고, 상위 노드에서 하위 노드의 이벤트를 제어 하는 방식이다.
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    // classList 메서드를 이용하여 classList를 배열처럼 가져올 수 있음
+    // 가능하면 innerText로 값을 비교하기 보다 element가 가지고있는 속성을 이용하는 것이 좋다.
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name'); // 클릭한 버튼을 기준 (수정버튼)을 기준으로 찾아가는게 좋다.
+
+      // 변수명이 길어도 의미가 명확하면 ok
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요',
+        $menuName.innerText
+      ); // 2번째 인자는 default 값
+
+      if (updatedMenuName === '') {
+        alert('수정값을 입력해주세요');
+        return;
+      }
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   //form 태그가 자동으로 전송되는 것을 막아준다.
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
